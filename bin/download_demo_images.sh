1fd9249d2005cc8f331048e9b2badefdd55c7585
teopost <teopost@gmail.com>
2015-08-09 13:44:38 +0200

    aggiunto script per scaricare immagini demo

---
+++ bin/download_demo_images.sh
@@ -0,0 +1,9 @@
+slides_folder=/home/pi/pi-kiosk/slides
+
+rm $slides_folder/*.jpg
+
+wget -O $slides_folder/image1.jpg http://images.wired.it/wp-content/uploads/2015/06/1435047714_Enterprise.jpg
+wget -O $slides_folder/image2.jpg http://culture.you-ng.it/wp-content/uploads/sites/3/2014/05/startrek.jpg 
+wget -O $slides_folder/image3.jpg http://media.insidepulse.com/zones/insidepulse/uploads/2014/05/2055-star-trek-wallpaper-1920x1080-wallpaper-5716.jpg
+wget -O $slides_folder/image4.jpg http://www.hollywoodreporter.com/sites/default/files/imagecache/gallery_landscape_1296x730/2015/02/Star_Trek_TV_Spock_2b.jpg
+wget -O $slides_folder/image5.jpg http://www.games969.com/wp-content/uploads/2013/02/star-trek-7.jpg
